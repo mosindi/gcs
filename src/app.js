97c9464618acc601c5246713cba8b7bce6b0bba4
Mustafa Ozan Şindi <ozan.sindi@ideaelearning.net>
2019-07-09 08:28:56 +0300

    reorganized

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const mongodb = require('./db/mongodb');
 const helper = require('./utils/helper')
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
