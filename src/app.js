55d355f9503aa32711cb5a607353a916e456afc3
Mustafa Ozan Şindi <ozan.sindi@ideaelearning.net>
2019-07-09 09:01:47 +0300

    creating index

--- src/app.js
+++ src/app.js
@@ -12,16 +12,21 @@ app.use(express.json());
 
 app.set('view engine', 'hbs');  //using Express.js view engine for handlebars.js to produce api pages
 
+app.get('',(req,res)=>{
+    res.render('index')
+})
 
 // endpoint to get records with search criterias
 app.post('/records', (req, res) => {
-    
-    helper.SearchRecordsParameterCheck(req.body.startDate, req.body.endDate, req.body.minCount,req.body.maxCount, (payloadError) => {
+
+    helper.SearchRecordsParameterCheck(req.body.startDate, req.body.endDate, req.body.minCount, req.body.maxCount, (payloadError) => {
+        // SearchRecordsParameter control the all of parameters exists. The method use also for business logic
         if (payloadError) {
-            res.send(result);
+            res.send(result);  // if payload paramaters have error return as result error.
         } else {
-            mongodb.SearchRecords(req.body.startDate, req.body.endDate, req.body.minCount,req.body.maxCount, (error, output) => {
-                var result = {};
+            mongodb.SearchRecords(req.body.startDate, req.body.endDate, req.body.minCount, req.body.maxCount, (error, output) => {
+                 //Mongo DB method to search and get records
+                
                 if (error) {
                     res.status(200).send({
                         "code": -1,
